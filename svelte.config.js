84ddd308489f2c8cb84454d977036c466517cf58
Christoph Hehl <christophhehl363@gmail.com>
2026-02-24 23:50:09 +0100

    ci: add action to deploy chillview to github pages

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,11 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
